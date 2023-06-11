9d12bca175628697909093b532f90679c5c6a156
kiteboy <lqfxz520@gmail.com>
2023-06-11 17:34:07 +0800

    feat: 动态虚拟列表，未完成

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { App } from 'vue'
 import VisualScrollList from './VisualScrollList'
+import DynamicVisualScrollList from './DynamicVisualScrollList'
 
 export default {
   install: (app: App) => {
     app.component('VisualScrollList', VisualScrollList)
+    app.component('DynamicVisualScrollList', DynamicVisualScrollList)
   },
 }
+
+// export { VisualScrollList, DynamicVisualScrollList }
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue-jsx'
-import externalGlobals from 'rollup-plugin-external-globals'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -10,9 +9,6 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
       vue(),
-      // externalGlobals({
-      //   vue: 'Vue',
-      // }),
     ],
     resolve: {
       alias: {
@@ -30,7 +26,7 @@ export default defineConfig(({ command, mode }) => {
       outDir: 'dist',
       lib: {
         entry: resolve(__dirname, 'src/index'),
-        name: 'VisualScrollList',
+        name: 'VisualScrollLib',
         fileName: 'visual-scroll-list',
       },
       rollupOptions: {
