27b5723d3cd8fec6a2ce0bffc418f623efd08d25
linqingfeng <lqfxz520@gmail.com>
2023-06-30 03:56:39 +0800

    feat: 完善动态滚动列表

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # Visual-Scroll-List-LQF
+
 虚拟列表插件
+
+### install
+
+```
+npm install visual-scroll-list-lqf
+```
+
+### usage
+
+- `ScrollVisualList` 固定高度虚拟列表组件
+
+| 属性       | 描述             | 类型   | 必须 | 默认 |
+| ---------- | ---------------- | ------ | ---- | ---- |
+| height     | 设置滚动列表高度 | number | 否   | 800  |
+| rowHeight  | 列高度           | number | 是   | -    |
+| total      | 列表总数量       | number | 是   | -    |
+| bufferSize | 显示视图外列数量 | number | 否   | 0    |
+
+```vue
+<template>
+  <ScrollVisualList :rowHeight="20" :total="listData.length">
+    <template #default="{ index }">
+      <span>{{ index }}</span>
+    </template>
+  </ScrollVisualList>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+import { ScrollVisualList } from 'scroll-visual-list-lqf'
+
+export default defineComponent({
+  setup() {
+    const listData = ref([])
+    listData.length = 1000
+    return {
+      listData,
+    }
+  },
+})
+</script>
+```
+
+- `DynamicScrollVisualList` 动态虚拟列表，支持非固定列高度使用
+
+| 属性              | 描述                               | 类型   | 必须 | 默认 |
+| ----------------- | ---------------------------------- | ------ | ---- | ---- |
+| height            | 设置滚动列表高度                   | number | 否   | 800  |
+| estimateRowHeight | 预设列高度，提供一个列高度的参考值 | number | 是   | -    |
+| total             | 列表总数量                         | number | 是   | -    |
+| bufferSize        | 显示视图外列数量                   | number | 否   | 0    |
+
+```vue
+<template>
+  <DynamicScrollVisualList :estimateRowHeight="20" :total="listData.length">
+    <template #default="{ index }">
+      <span>{{ index }}</span>
+    </template>
+  </DynamicScrollVisualList>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+import { DynamicScrollVisualList } from 'scroll-visual-list-lqf'
+
+export default defineComponent({
+  setup() {
+    const listData = ref([])
+    listData.length = 1000
+    return {
+      listData,
+    }
+  },
+})
+</script>
+```
--- src/DynamicVisualScrollList.tsx
+++ src/DynamicVisualScrollList.tsx
@@ -1,162 +1,157 @@
-import { defineComponent, onMounted, onUpdated, ref, toRefs } from 'vue'
+import {
+  defineComponent,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+  StyleValue,
+  toRefs,
+} from 'vue'
 
 interface CachePosition {
   index: number
   top: number
   bottom: number
-  height: number
-  dValue: number
+  itemHeight: number
 }
+
+const ListItem = defineComponent({
+  emits: ['update:meta'],
+  setup(props, { slots, emit }) {
+    const divRef = ref<HTMLDivElement>()
+    let resizeObserver: ResizeObserver
+    onMounted(() => {
+      resizeObserver = new ResizeObserver(() => {
+        emit('update:meta', divRef.value)
+      })
+      resizeObserver.observe(divRef.value as HTMLDivElement)
+    })
+    onBeforeUnmount(() => {
+      resizeObserver.unobserve(divRef.value as HTMLDivElement)
+    })
+    return () => {
+      return (
+        <div ref={divRef}>
+          {slots.default ? slots.default({ ...props }) : ''}
+        </div>
+      )
+    }
+  },
+})
 
 export default defineComponent({
   props: {
-    total: {
-      type: Number,
-      required: true,
-    },
-    estimateRowHeight: {
+    estimatedHeight: {
       type: Number,
       required: true,
     },
     height: {
       type: Number,
-      default: 800,
+      default: 600,
     },
-    tag: {
-      type: String,
-      default: 'div',
-    },
-    bufferSize: {
-      type: Number,
-      default: 0,
+    listItems: {
+      type: Array,
+      required: true,
     },
   },
   setup(props, { slots }) {
-    const { total, estimateRowHeight, height, bufferSize } = toRefs(props)
-
-    const scrollTop = ref<number>(0)
-    const limit = ref<number>(Math.ceil(height.value / estimateRowHeight.value))
-    const originHeight = ref<number>(estimateRowHeight.value * total.value)
-    const originStartIndex = ref<number>(0)
-    const cachePositions = ref<CachePosition[]>([])
-    const scorllContainerRef = ref<HTMLElement>()
-    const barRef = ref<HTMLElement>()
-    const absoluteContainerRef = ref<HTMLElement>()
+    // 获取容器
+    const listContainer = ref<HTMLDivElement>()
+    // 在可视区域渲染的列表项
+    const items = ref<unknown[]>([])
+    const itemsMetaData = ref<CachePosition[]>([])
     const startIndex = ref<number>(0)
-    const endIndex = ref<number>(0)
 
-    const initCachePositions = () => {
-      const positions = []
-      for (let i = 1; i <= total.value; i++) {
-        positions.push({
-          index: i - 1,
-          bottom: estimateRowHeight.value * i,
-          top: estimateRowHeight.value * (i - 1),
-          dValue: 0,
-          height: estimateRowHeight.value,
-        })
-      }
-      cachePositions.value = positions
-    }
-
-    const updateCachePositions = () => {
-      // 如何找出元素索引
-      const nodes = absoluteContainerRef.value?.children
-      if (!nodes) return
-      // const firstNode  = nodes[0]
-
-      for (const node of nodes) {
-        const { height } = node.getBoundingClientRect()
-        console.log(node)
-      }
-    }
-
-    const getStartIndex = (scrollTop = 0) => {
-      let idx = cachePositions.value.findIndex((position, index) => {
-        if (scrollTop - position.top) {
-          // not do anything
-        } else {
-          return index
+    // 假设获取每个列表项的高度的函数为 getItemHeight(index)
+    const setItemsMetaData = () => {
+      // const visibleItemCount = Math.ceil(props.height / props.estimatedHeight)
+      const estimatedHeight = props.estimatedHeight
+      itemsMetaData.value = props.listItems.map((_value, index) => {
+        return {
+          index,
+          itemHeight: estimatedHeight,
+          top: estimatedHeight * index,
+          bottom: estimatedHeight * (index + 1),
         }
       })
+    }
 
-      // 如果超出滚动的高度，说明存在一部分空白
-      // 则多显示一列
-      if (scrollTop < cachePositions.value[idx].top) {
-        idx = idx - 1
+    // 初始化虚拟滚动列表
+    const initVirtualScroll = () => {
+      setItemsMetaData()
+      // 更新容器的高度为所有列表项高度之和
+      updateVisibleItems()
+    }
+
+    // 更新可见区域的列表项
+    const updateVisibleItems = () => {
+      // 获取容器的滚动偏移量和可视区域高度
+      const scrollTop = listContainer.value ? listContainer.value.scrollTop : 0
+      // 计算可见区域的起始索引和结束索引
+      const toItemsMetaData = itemsMetaData.value
+      let start = 0
+      let end = toItemsMetaData.length
+      for (let i = 0; i < toItemsMetaData.length; i++) {
+        if (toItemsMetaData[i].bottom >= scrollTop) {
+          start = i
+          break
+        }
       }
-      return idx
-    }
 
-    const onScroll = (event: Event) => {
-      const bScrollTop = (event.target as HTMLDivElement).scrollTop
-      const currentIndex = getStartIndex(bScrollTop)
-
-      if (originStartIndex.value !== currentIndex) {
-        originStartIndex.value = currentIndex
-        startIndex.value = Math.max(currentIndex - bufferSize.value, 0)
-        endIndex.value = Math.min(
-          currentIndex + limit.value + bufferSize.value,
-          total.value
-        )
-
-        scrollTop.value = bScrollTop
+      end = start
+      while (
+        end <= toItemsMetaData.length &&
+        toItemsMetaData[end].bottom < scrollTop + props.height
+      ) {
+        end++
       }
+      // 渲染可见区域的列表项
+      items.value = props.listItems.slice(start, end + 1)
+      startIndex.value = start
     }
 
-    const getTransform = () => {
-      return `
-              translateY(${cachePositions.value[startIndex.value].top}px)
-            `
+    const sizeChangeHandle = (index: number, target: HTMLDivElement) => {
+      const { top, bottom, height } = target.getBoundingClientRect()
+      console.log(index, top)
+      itemsMetaData.value[index] = { index, top, bottom, itemHeight: height }
     }
 
-    initCachePositions()
-
-    onMounted(() => {
-      updateCachePositions()
-    })
-
-    onUpdated(() => {
-      updateCachePositions()
-    })
+    initVirtualScroll()
 
     return () => {
-      const redrawRender = () => {
-        const elements = []
-        for (let i = startIndex.value; i <= endIndex.value; i++) {
-          elements.push(slots.default!({ index: i }))
-        }
-        return elements
-      }
-
+      const { length } = itemsMetaData.value
+      const areaHeight = length ? itemsMetaData.value[length - 1].bottom : 0
+      const top = length ? itemsMetaData.value[startIndex.value].top : 0
       return (
         <div
-          ref={scorllContainerRef}
+          onScroll={updateVisibleItems}
+          ref={listContainer}
           style={{
             position: 'relative',
-            overflow: 'hidden auto',
-            height: `${height.value}px`,
+            overflow: 'auto',
+            height: `${props.height}px`,
           }}
-          onScroll={onScroll}
         >
+          <div style={{ height: `${areaHeight}px` }} />
           <div
-            ref={barRef}
-            style={{
-              position: 'relative',
-              top: 0,
-              height: `${originHeight.value}px`,
-            }}
-          />
-          <div
-            ref={absoluteContainerRef}
             style={{
               position: 'absolute',
               top: 0,
               width: '100%',
-              transform: getTransform(),
+              transform: `translateY(${top}px)`,
             }}
           >
-            {redrawRender()}
+            {items.value.map((item, index) => {
+              return (
+                <ListItem
+                  onUpdate:meta={(target: HTMLDivElement) => {
+                    sizeChangeHandle(startIndex.value + index, target)
+                  }}
+                  key={startIndex.value + index}
+                >
+                  {slots.default ? slots.default({ item }) : ''}
+                </ListItem>
+              )
+            })}
           </div>
         </div>
       )
